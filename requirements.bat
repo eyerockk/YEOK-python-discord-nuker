fdb0920dedcda01b501b1bfcdbd722f132058fa4
eyerockk <101639032+eyerockk@users.noreply.github.com>
2022-11-12 11:39:58 -0500

    Create requirements.bat

---
+++ requirements.bat
@@ -0,0 +1,11 @@
+pip install requests
+pip install asyncio
+pip install aiohttp
+pip install discord.py
+pip install colorama
+
+python3 -m pip install colorama
+python3 -m pip install discord.py
+python3 -m pip install aiohttp
+python3 -m pip install asyncio
+python3 -m pip install requests
